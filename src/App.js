709a9bbec289bb144c30a57d4bc82d572b8aee21
Eliseo Diaz Medina <eliseodm@gmail.com>
2020-06-02 04:39:34 -0300

    now impolement Exercises component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import User from './components/User'
+import Exercise from './components/Exercise';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <p> Edit <code>src/App.js</code> and save to reload.</p>
         <User />
+        <Exercise />
       </header>
     </div>
   );
